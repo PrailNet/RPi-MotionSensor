5f6fbf8a118b1f67a9220c1d5c34a4bf8bab926d
Noah Prail <nprail@filiosoft.com>
2016-12-28 10:38:35 -0500

    Removed NodeMailer. That was a fun experiment.

--- app.js
+++ app.js
@@ -1,31 +1,7 @@
 var Gpio = require('onoff').Gpio;
 var sh = require('shelljs');
-var nodemailer = require('nodemailer');
-var sgTransport = require('nodemailer-sendgrid-transport');
 // Configure GPIO pin 21 for input and rising edge detection
 var pir = new Gpio(21, 'in', 'rising');
-var sgOptions = {
-    auth: {
-        api_user: 'iot@nprail.me',
-        api_key: 'eR1s2I9q2lAN'
-    }
-}
-var mailer = nodemailer.createTransport(sgTransport(sgOptions));
-
-// Verify Email Settings
-mailer.verify(function (error, success) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Server is ready to take our messages');
-    }
-});
-var email = {
-    to: 'noah@prail.net',
-    from: 'iot@nprail.me',
-    subject: 'Prail.Net IoT Message: Motion Detected',
-    text: 'Motion has been detected on RPiS1.'
-};
 
 // Add the edge detection callback to catch the motion detection events
 pir.watch(function (err, value) {
@@ -35,12 +11,6 @@ pir.watch(function (err, value) {
             console.log('Exit code:', code);
             console.log('Program output:', stdout);
             console.log('Program stderr:', stderr);
-        });
-        mailer.sendMail(email, function (err, res) {
-            if (err) {
-                console.log(err)
-            }
-            console.log(res);
         });
         console.log("Motion Detected: %d", value);
     }
