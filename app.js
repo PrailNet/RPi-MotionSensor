d909fc4afa743ec8f8db25be62bc6491051137b3
Noah Prail <nprail@filiosoft.com>
2016-12-27 22:36:25 -0500

    Added ShellJS...

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 var Gpio = require('onoff').Gpio;
+var sh = require('shelljs');
 // Configure GPIO pin 21 for input and rising edge detection
-var pir = new Gpio(21,'in','rising');
+var pir = new Gpio(21, 'in', 'rising');
 
 // Add the edge detection callback to catch the motion detection events
-pir.watch(function(err, value) {
-  if (value === 1) {
-    // The pin went high
-    console.log("Motion Detected: %d", value);
-  }
+pir.watch(function (err, value) {
+    if (value === 1) {
+        // The pin went high
+        sh.exec('bash screenOn.sh', function (code, stdout, stderr) {
+            console.log('Exit code:', code);
+            console.log('Program output:', stdout);
+            console.log('Program stderr:', stderr);
+        });
+        console.log("Motion Detected: %d", value);
+    }
 });
 
 
 function exit() {
-  console.log("Exiting");
-  pir.unexport();
-  process.exit();
+    console.log("Exiting");
+    pir.unexport();
+    process.exit();
 }
 
 process.on('SIGINT', exit);
