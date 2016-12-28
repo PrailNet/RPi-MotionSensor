f996b09444790ed7f11767f5fc33d3a076aed676
Noah Prail <nprail@filiosoft.com>
2016-12-28 10:13:29 -0500

    Added Email stuff.

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 var Gpio = require('onoff').Gpio;
 var sh = require('shelljs');
+var nodemailer = require('nodemailer');
 // Configure GPIO pin 21 for input and rising edge detection
 var pir = new Gpio(21, 'in', 'rising');
+var smtpConfig = {
+    host: 'smtp.sendgrid.net',
+    port: 587,
+    secure: true, // use SSL
+    auth: {
+        user: 'IoT@NPrail.me',
+        pass: 'eR1s2I9q2lAN'
+    }
+};
+var transporter = nodemailer.createTransport(options[smtpConfig]);
+
+// Verify Email Settings
+transporter.verify(function(error, success) {
+   if (error) {
+        console.log(error);
+   } else {
+        console.log('Server is ready to take our messages');
+   }
+});
 
 // Add the edge detection callback to catch the motion detection events
 pir.watch(function (err, value) {
